4bd8f344132a4f674f12fbd599b31ea94c1b921e
salcudeann <adi.salcudeann@yahoo.com>
2022-01-05 20:37:39 +0200

    Create ad model

---
+++ LostPet/Models/AdModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LostPet.Models
+{
+    public class AdModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string Owner { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool Rejected { get; set; }
+    }
+}
