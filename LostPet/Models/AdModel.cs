8c89e6b1336efebbfd67b434197fa89b3f581ea9
salcudeann <adi.salcudeann@yahoo.com>
2022-01-06 19:45:03 +0200

    wip

---
+++ LostPet/Models/AdModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LostPet.Models
+{
+    public class AdModel
+    {
+        [Display(Name = "Ad title")]
+        [Required(ErrorMessage = "Ad title is mandatory")]
+        public string Title { get; set; }
+
+        [Display(Name = "Ad description")]
+        [Required(ErrorMessage = "Ad description is mandatory")]
+        [StringLength(500, MinimumLength = 20, ErrorMessage = "Ad description should have at least 20 characters.")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime PublishDate { get; set; }
+
+        [Display(Name = "Place of loss")]
+        [StringLength(50)]
+        public string Location { get; set; }
+
+        public string UserName { get; set; }
+
+        [Display(Name = "Images")]
+        public List<string> Images { get; set; }
+
+        public bool Status { get; set; } //false - pending approval; true - approved
+
+    }
+}
